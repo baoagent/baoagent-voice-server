f8b00094e221b0d198ecfaf95bfbf0cda0b75ea4
Kevin Su <kevinsu2024@u.northwestern.edu>
2025-07-20 23:25:45 -0400

    gitignore

--- dist/utils/audio-utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertMulawToPCM = void 0;
-const alawmulaw_1 = require("alawmulaw");
-const convertMulawToPCM = (mulawData) => {
-    const pcmData = alawmulaw_1.mulaw.decode(mulawData);
-    return Buffer.from(pcmData.buffer);
-};
-exports.convertMulawToPCM = convertMulawToPCM;
